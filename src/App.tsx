4614e4690b33a48f11a003e19ac04f403033afca
Antony1060 <antoniostignjedec@gmail.com>
2022-02-10 23:43:17 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Root, Head } from 'react-static'
+
+function App() {
+    return (
+        <Root>
+            <Head>
+                <meta name="test" content="sex" />
+            </Head>
+            <div className="content">
+                <h1>sex</h1>
+            </div>
+        </Root>
+    )
+}
+
+export default App
