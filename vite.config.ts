a3141d6b4a2e888977f79708444c3a33ef6e78fb
Antony1060 <antoniostignjedec@gmail.com>
2022-02-24 12:13:39 +0100

    Compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import compress from "vite-plugin-compression"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    compress({ algorithm: "brotliCompress" }),
     {
       name: "remove-preload",
       enforce: "post",
       transformIndexHtml: (html) =>
         html.replace(/\s*(<link rel="modulepreload"\s+href=".*vendor.*\.js".*>)\s*/gi, "")
     }
-  ]
+  ],
+  build: {
+    minify: "esbuild"
+  }
 })
