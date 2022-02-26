ebbc9753dc381856ec366cc84c3d99bbe793d3f0
Antony1060 <antoniostignjedec@gmail.com>
2022-02-26 14:05:46 +0100

    Style scroll

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -13,6 +13,15 @@ const GlobalStyles = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+
+        ::-webkit-scrollbar {
+            background-color: #151a22;
+        }
+        
+        ::-webkit-scrollbar-thumb {
+            background-color: #272b33;
+        }
+
     }
 
     body {
