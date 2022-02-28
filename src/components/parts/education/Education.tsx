955b8fc818d0f253c7c39e10bc9df871f26416e8
Antony1060 <antoniostignjedec@gmail.com>
2022-02-28 18:14:21 +0100

    Info on education

--- src/components/parts/education/Education.tsx
+++ src/components/parts/education/Education.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { Info } from "react-feather";
 import styled from "styled-components";
 import SectionContainer from "../../elements/SectionContainer";
 import EducationPoint from "./EducationPoint";
@@ -18,11 +19,57 @@ const Connector = styled.div<{ status: EducationStatus }>`
     flex-grow: 1;
     margin: 0 -10px;
     background-color: ${({ status }) => status === "passed" ? "#3f5c96" : status === "active" ? "#55e480" : "#272b33"};
+    position: relative;
 `;
+
+const InfoContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    position: relative;
+
+    &:hover div {
+        visibility: visible;
+    }
+`;
+
+const InfoHover = styled.div`
+    position: absolute;
+    left: 50%;
+    top: calc(-100% - 1.4rem);
+    display: flex;
+    width: 200px;
+    transform: translateX(-50%) translateY(-50%);
+    visibility: hidden;
+    background-color: #282c32ee;
+    border: 1px solid gray;
+    padding: 0.4rem;
+    border-radius: 4px;
+    text-align: center;
+    font-size: 0.9rem;
+    z-index: 10;
+
+    @media (max-width: 1050px) {
+        left: calc(200% + 0.6rem);
+        transform: translateX(-100%) translateY(-50%);
+    }
+`;
+
+const InfoAlert: FC = () => {
+    return (
+        <InfoContainer>
+            <Info size={20} />
+            <InfoHover>
+                All information is displayed in accordance with the Croatian school system.
+            </InfoHover>
+        </InfoContainer>
+    )
+}
 
 const Education: FC = () => {
     return (
-        <SectionContainer name="Education">
+        <SectionContainer name="Education" after={<InfoAlert />}>
             <Container>
                 <EducationPoint
                     status="passed"
