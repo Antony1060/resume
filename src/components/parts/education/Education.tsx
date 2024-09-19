35cfef78995010d396190a3d6396e835e6fb4698
Antonio F. Trstenjak <antoniostignjedec@gmail.com>
2024-09-19 21:38:13 +0200

    Update ECTS

--- src/components/parts/education/Education.tsx
+++ src/components/parts/education/Education.tsx
@@ -78,7 +78,7 @@ const InfoAlert: FC = () => {
     );
 };
 
-const [UNI_ECTS, UNI_ECTS_MAX] = [19, 180] as const;
+const [UNI_ECTS, UNI_ECTS_MAX] = [51, 180] as const;
 
 const Education: FC = () => {
     return (
