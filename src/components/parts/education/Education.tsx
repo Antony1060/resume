b805db4b20d8a98d4825694f2e0f97dea468fd34
Antonio F. Trstenjak <antoniostignjedec@gmail.com>
2026-02-04 16:02:18 +0100

    Update

--- src/components/parts/education/Education.tsx
+++ src/components/parts/education/Education.tsx
@@ -7,7 +7,7 @@ import { Percent } from "../../../lib/Percent";
 import SectionContainer from "../../elements/SectionContainer";
 import EducationPoint from "./EducationPoint";
 
-const [UNI_ECTS, UNI_ECTS_MAX] = [115, 180] as const;
+const [UNI_ECTS, UNI_ECTS_MAX] = [136, 180] as const;
 
 const Container = styled.div`
     display: flex;
